efed1b1f7e1f51be04c8fc2211419215880d2ce0
milafrerichs <mila.frerichs@gmail.com>
2018-02-25 10:26:01 +0100

    Add more cities to harvester;
    add more new data to city creation file

--- cities_details.rb
+++ cities_details.rb
@@ -1,12 +1,30 @@
 require 'json'
 
+def write_if_not_exists(filename, &content_block)
+  unless File.file?(filename)
+    puts "write file #{filename}"
+    open(filename, 'w') { |f|
+      content_block.call(f)
+    }
+  end
+end
+
 file = File.read('get_all_cities.json')
 data_hash = JSON.parse(file)
 data_hash["cities"].each do |city|
-  open("_cities/#{city}.md", 'w') { |f|
-    f << "---\n"
-    f << "city: #{city}\n"
-    f << "layout: city\n"
-    f << "---\n"
-  }
+  city_detail_site = "_cities/#{city}.md"
+  write_if_not_exists(city_detail_site) do |f|
+      f << "---\n"
+      f << "city: #{city}\n"
+      f << "layout: city\n"
+      f << "---\n"
+  end
+  city_stats = "_data/city_stats/#{city}.csv"
+  write_if_not_exists(city_stats) do |f|
+      f << ""
+  end
+  city_datasets = "_data/city_stats/datasets/#{city}.json"
+  write_if_not_exists(city_datasets) do |f|
+      f << ""
+  end
 end
